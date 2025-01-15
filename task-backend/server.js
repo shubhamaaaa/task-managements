310f1eacf03990dc91e1c9a4706ff8234f34e3d3
shubhamaaaa <130844125+shubhamaaaa@users.noreply.github.com>
2025-01-15 16:45:49 +0530

    Update server.js

--- task-backend/server.js
+++ task-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"], 
+    origin: ["http://localhost:5000", "http://localhost:5173"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
@@ -68,7 +68,7 @@ io.on("connection", (socket) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the School Management API!');
+    res.send('Welcome to the Task Management API!');
 });
 // Start Server
 const PORT = process.env.PORT || 5000;
